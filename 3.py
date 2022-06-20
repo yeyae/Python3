bb916753a80b483a95fb207002045432b70f43c6
yeyae <whyjinny@naver.com>
2022-06-20 09:12:11 +0900

    python

---
+++ 3.py
@@ -0,0 +1,18 @@
+#나눗셈 몫을 반환하는 연산자: //
+7//4
+
+#문자열을 만드는 방법 : "", '', """""",''''''
+"""Life is too short, You need python"""
+
+#문자열에 작은 따옴표(') 포함시키는 방법
+#Python's favorite food is perl
+#큰 따옴표로 둘러싸게 만들기
+food = "Python's favorite food is perl"
+food
+
+#\를 사용해서 '와 "를 문자열에 포함시키기
+food = 'python\'s favorite food is perl'
+say= "\"Python is very easy.\"he says."
+food
+say
+
